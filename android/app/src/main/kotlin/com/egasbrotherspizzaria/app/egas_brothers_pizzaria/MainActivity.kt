bd5bcc5aa23811ffae5de749d440f7e41b02e379
Av3z <willyanf17@hotmail.com>
2024-01-25 21:37:46 -0300

    config: configuração inicial do projeto

---
+++ android/app/src/main/kotlin/com/egasbrotherspizzaria/app/egas_brothers_pizzaria/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.egasbrotherspizzaria.app.egas_brothers_pizzaria
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:egas_brothers_pizzaria/src/app.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(App());
+}
