574b8579529be5aff7dd079e3fa550bbe191e8e8
Av3z <willyanf17@hotmail.com>
2024-02-10 23:14:38 -0300

    feat: add auth manager page

--- lib/src/pages/auth_page.dart
+++ lib/src/pages/auth_page.dart
@@ -1,14 +1,25 @@
 import 'package:egas_brothers_pizzaria/src/pages/home_page.dart';
 import 'package:egas_brothers_pizzaria/src/pages/login_page.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-class AuthPage extends StatelessWidget {
+class AuthPage extends StatefulWidget {
   const AuthPage({super.key});
 
+  @override
+  State<AuthPage> createState() => _AuthPageState();
+}
+
+class _AuthPageState extends State<AuthPage> {
   final bool isLogged = false;
 
   @override
   Widget build(BuildContext context) {
-    return isLogged ? const HomePage() : const LoginPage();
+    return StreamBuilder(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        return snapshot.hasData ? const HomePage() : const LoginPage();
+      },
+    );
   }
 }
