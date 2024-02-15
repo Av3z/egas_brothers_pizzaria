c7f5dfe9a198283b80cc15b4dd987b28f6124290
Av3z <willyanf17@hotmail.com>
2024-02-15 00:14:19 -0300

    feat: add manager page

---
+++ lib/src/pages/manager_page.dart
@@ -0,0 +1,40 @@
+import 'package:egas_brothers_pizzaria/src/components/bottom_nav.dart';
+import 'package:egas_brothers_pizzaria/src/pages/cart_page.dart';
+import 'package:egas_brothers_pizzaria/src/pages/coupon_page.dart';
+import 'package:egas_brothers_pizzaria/src/pages/home_page.dart';
+import 'package:egas_brothers_pizzaria/src/pages/order_page.dart';
+import 'package:egas_brothers_pizzaria/src/pages/profile_page.dart';
+import 'package:flutter/material.dart';
+
+class ManagerPage extends StatefulWidget {
+  const ManagerPage({super.key});
+
+  @override
+  State<ManagerPage> createState() => _ManagerPageState();
+}
+
+class _ManagerPageState extends State<ManagerPage> {
+  int pageIndex = 2;
+
+  List<Widget> pages = [
+    const OrderPage(),
+    const CouponPage(),
+    const HomePage(),
+    const CartPage(),
+    const ProfilePage()
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: BottomNav(
+        onTap: (value) {
+          setState(() {
+            pageIndex = value;
+          });
+        },
+      ),
+      body: pages[pageIndex],
+    );
+  }
+}
