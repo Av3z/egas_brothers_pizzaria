37f14b6f0f4cd0e6b8df19d170961a611bea46cf
Av3z <willyanf17@hotmail.com>
2024-02-19 19:18:21 -0300

    feat: add get foods to firebase

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,11 +1,31 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:egas_brothers_pizzaria/src/components/home_components/card_item_widget.dart';
 import 'package:egas_brothers_pizzaria/src/components/home_components/header_widget.dart';
 import 'package:egas_brothers_pizzaria/src/components/home_components/menu_category.dart';
+import 'package:egas_brothers_pizzaria/src/models/food_model.dart';
+import 'package:egas_brothers_pizzaria/src/services/food_service.dart';
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
-  final String client = "Willyan";
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final String client = "Willyan";
+  final FoodService _foodService = FoodService();
+
+  final ScrollController _controller = ScrollController();
+
+  void scrollTo(double position) {
+    _controller.animateTo(
+      position,
+      duration: const Duration(milliseconds: 500),
+      curve: Curves.easeInOut,
+    );
+  }
 
   @override
   Widget build(context) {
@@ -32,6 +52,7 @@ class HomePage extends StatelessWidget {
                   MenuCategory(
                       text: 'Tudo',
                       imagePath: 'lib/images/categories/cat_all.png'),
+
                   // OFFERS
                   MenuCategory(
                       text: 'Ofertas',
@@ -67,18 +88,27 @@ class HomePage extends StatelessWidget {
 
                 // Content
                 SizedBox(
-                  height: 250,
-                  child: ListView(
-                    scrollDirection: Axis.horizontal,
-                    children: const [
-                      CardItemWidget(),
-                      CardItemWidget(),
-                      CardItemWidget(),
-                      CardItemWidget(),
-                      CardItemWidget(),
-                    ],
-                  ),
-                ),
+                    height: 270,
+                    child: StreamBuilder<QuerySnapshot>(
+                      stream: _foodService.getAllFoods('Ofertas'),
+                      builder: (context, snapshot) {
+                        return ListView(
+                          scrollDirection: Axis.horizontal,
+                          children: snapshot.data!.docs.map((e) {
+                            var foodModel = FoodModel.fromJson(
+                                (e.data() as Map<String, dynamic>));
+
+                            return CardItemWidget(
+                              title: foodModel.title,
+                              description: foodModel.description,
+                              peoples: foodModel.peoples,
+                              price: foodModel.price,
+                              priceColor: const Color(0xFF2E9048),
+                            );
+                          }).toList(),
+                        );
+                      },
+                    )),
               ],
             ),
 
@@ -95,18 +125,26 @@ class HomePage extends StatelessWidget {
 
                 // Content
                 SizedBox(
-                  height: 250,
-                  child: ListView(
-                    scrollDirection: Axis.horizontal,
-                    children: const [
-                      CardItemWidget(),
-                      CardItemWidget(),
-                      CardItemWidget(),
-                      CardItemWidget(),
-                      CardItemWidget(),
-                    ],
-                  ),
-                ),
+                    height: 270,
+                    child: StreamBuilder<QuerySnapshot>(
+                      stream: _foodService.getAllFoods('Pizzas'),
+                      builder: (context, snapshot) {
+                        return ListView(
+                          scrollDirection: Axis.horizontal,
+                          children: snapshot.data!.docs.map((e) {
+                            var foodModel = FoodModel.fromJson(
+                                (e.data() as Map<String, dynamic>));
+
+                            return CardItemWidget(
+                              title: foodModel.title,
+                              description: foodModel.description,
+                              peoples: foodModel.peoples,
+                              price: foodModel.price,
+                            );
+                          }).toList(),
+                        );
+                      },
+                    )),
               ],
             ),
             const SizedBox(
@@ -124,18 +162,64 @@ class HomePage extends StatelessWidget {
 
                 // Content
                 SizedBox(
-                  height: 250,
-                  child: ListView(
-                    scrollDirection: Axis.horizontal,
-                    children: const [
-                      CardItemWidget(),
-                      CardItemWidget(),
-                      CardItemWidget(),
-                      CardItemWidget(),
-                      CardItemWidget(),
-                    ],
-                  ),
+                    height: 270,
+                    child: StreamBuilder<QuerySnapshot>(
+                      stream: _foodService.getAllFoods('Bebidas'),
+                      builder: (context, snapshot) {
+                        return ListView(
+                          scrollDirection: Axis.horizontal,
+                          children: snapshot.data!.docs.map((e) {
+                            var foodModel = FoodModel.fromJson(
+                                (e.data() as Map<String, dynamic>));
+
+                            return CardItemWidget(
+                              title: foodModel.title,
+                              description: foodModel.description,
+                              peoples: foodModel.peoples,
+                              price: foodModel.price,
+                            );
+                          }).toList(),
+                        );
+                      },
+                    )),
+              ],
+            ),
+
+            const SizedBox(
+              height: 15,
+            ),
+
+            // PORÇÕES
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  'Porções',
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
                 ),
+
+                // Content
+                SizedBox(
+                    height: 270,
+                    child: StreamBuilder<QuerySnapshot>(
+                      stream: _foodService.getAllFoods('Porções'),
+                      builder: (context, snapshot) {
+                        return ListView(
+                          scrollDirection: Axis.horizontal,
+                          children: snapshot.data!.docs.map((e) {
+                            var foodModel = FoodModel.fromJson(
+                                (e.data() as Map<String, dynamic>));
+
+                            return CardItemWidget(
+                              title: foodModel.title,
+                              description: foodModel.description,
+                              peoples: foodModel.peoples,
+                              price: foodModel.price,
+                            );
+                          }).toList(),
+                        );
+                      },
+                    )),
               ],
             ),
           ],
