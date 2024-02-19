73e9d930b2cfa8a9df319541020c10d772be384d
Av3z <willyanf17@hotmail.com>
2024-02-19 19:17:23 -0300

    feat: make dynamic card item widget

--- lib/src/components/home_components/card_item_widget.dart
+++ lib/src/components/home_components/card_item_widget.dart
@@ -1,13 +1,24 @@
 import 'package:flutter/material.dart';
 
 class CardItemWidget extends StatelessWidget {
-  const CardItemWidget({super.key});
+  final String title;
+  final String description;
+  final String peoples;
+  final double price;
+  final Color priceColor;
+
+  const CardItemWidget(
+      {super.key,
+      required this.title,
+      required this.price,
+      required this.description,
+      required this.peoples,
+      this.priceColor = Colors.black});
 
   @override
   Widget build(BuildContext context) {
     return Container(
       margin: const EdgeInsets.only(right: 15, top: 10, bottom: 10),
-      alignment: Alignment.centerLeft,
       decoration: BoxDecoration(
           color: Colors.white,
           borderRadius: BorderRadius.circular(8),
@@ -18,60 +29,55 @@ class CardItemWidget extends StatelessWidget {
                 color: Colors.grey.shade300,
                 offset: const Offset(0, 3)),
           ]),
-      padding: const EdgeInsets.all(10),
-      width: 150,
+      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+      width: 155,
       child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Image.asset('lib/images/Logo.png'),
           const SizedBox(
             height: 10,
           ),
-          const Text(
-            'Titulo',
-            style: TextStyle(fontWeight: FontWeight.w600, fontSize: 14),
+          Text(
+            title,
+            style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 14),
           ),
-          const SizedBox(
-            height: 20,
-          ),
-          const Text('Descrição do produto',
-              style: TextStyle(
+          Text(description,
+              maxLines:
+                  2, // Defina o número máximo de linhas que o texto pode ocupar
+              overflow: TextOverflow.ellipsis,
+              style: const TextStyle(
                   fontSize: 12,
                   color: Color(
                     0xFF696969,
                   ),
                   fontWeight: FontWeight.w600)),
-          const SizedBox(
-            height: 5,
-          ),
-          const Row(
+          Row(
             children: [
               Text(
-                '2-3',
-                style: TextStyle(
+                peoples,
+                style: const TextStyle(
                     color: Color(0xFF696969), fontWeight: FontWeight.w600),
               ),
-              SizedBox(
+              const SizedBox(
                 width: 5,
               ),
-              Icon(
+              const Icon(
                 Icons.people_rounded,
                 color: Color(0xFF696969),
               )
             ],
           ),
-          const SizedBox(
-            height: 10,
-          ),
-          const Row(
+          Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               Text(
-                'R\$ 59,90',
+                'R\$ $price',
                 style:
-                    TextStyle(color: Colors.green, fontWeight: FontWeight.bold),
+                    TextStyle(color: priceColor, fontWeight: FontWeight.bold),
               ),
-              Icon(Icons.add_shopping_cart),
+              const Icon(Icons.add_shopping_cart),
             ],
           ),
         ],
