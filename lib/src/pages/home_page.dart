777e7088a75a3046642cb9a63b8d715ada088028
Av3z <willyanf17@hotmail.com>
2024-02-20 12:41:04 -0300

    feat: get all itens from firebase

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -3,6 +3,7 @@ import 'package:egas_brothers_pizzaria/src/components/home_components/card_item_
 import 'package:egas_brothers_pizzaria/src/components/home_components/header_widget.dart';
 import 'package:egas_brothers_pizzaria/src/components/home_components/menu_category.dart';
 import 'package:egas_brothers_pizzaria/src/models/food_model.dart';
+import 'package:egas_brothers_pizzaria/src/pages/details_page.dart';
 import 'package:egas_brothers_pizzaria/src/services/food_service.dart';
 import 'package:flutter/material.dart';
 
@@ -16,16 +17,6 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   final String client = "Willyan";
   final FoodService _foodService = FoodService();
-
-  final ScrollController _controller = ScrollController();
-
-  void scrollTo(double position) {
-    _controller.animateTo(
-      position,
-      duration: const Duration(milliseconds: 500),
-      curve: Curves.easeInOut,
-    );
-  }
 
   @override
   Widget build(context) {
@@ -63,12 +54,12 @@ class _HomePageState extends State<HomePage> {
                       imagePath: 'lib/images/categories/cat_pizzas.png'),
                   // Bebidas
                   MenuCategory(
-                      text: 'Bebidas',
-                      imagePath: 'lib/images/categories/cat_drinks.png'),
-                  // Porções
-                  MenuCategory(
                       text: 'Porções',
                       imagePath: 'lib/images/categories/cat_portions.png'),
+                  // Porções
+                  MenuCategory(
+                      text: 'Bebidas',
+                      imagePath: 'lib/images/categories/cat_drinks.png'),
                 ],
               ),
             ),
@@ -79,149 +70,208 @@ class _HomePageState extends State<HomePage> {
 
             // Ofertas
             Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                const Text(
-                  'Ofertas',
-                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      'Ofertas',
+                      style:
+                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                    ),
+
+                    // Content
+                    SizedBox(
+                        height: 270,
+                        child: StreamBuilder<QuerySnapshot>(
+                          stream: _foodService.getAllFoods('Ofertas'),
+                          builder: (context, snapshot) {
+                            return ListView(
+                              scrollDirection: Axis.horizontal,
+                              children: snapshot.data!.docs.map((e) {
+                                var foodModel = FoodModel.fromJson(
+                                    (e.data() as Map<String, dynamic>));
+
+                                return GestureDetector(
+                                  key: Key(foodModel.imageId),
+                                  onTap: () {
+                                    Navigator.push(context, MaterialPageRoute(
+                                      builder: (context) {
+                                        return DetailsPage(
+                                          foodModel: foodModel,
+                                        );
+                                      },
+                                    ));
+                                  },
+                                  child: CardItemWidget(
+                                    title: foodModel.title,
+                                    imageId: foodModel.imageId,
+                                    description: foodModel.description,
+                                    peoples: foodModel.peoples,
+                                    price: foodModel.price,
+                                  ),
+                                );
+                              }).toList(),
+                            );
+                          },
+                        )),
+                  ],
                 ),
 
-                // Content
-                SizedBox(
-                    height: 270,
-                    child: StreamBuilder<QuerySnapshot>(
-                      stream: _foodService.getAllFoods('Ofertas'),
-                      builder: (context, snapshot) {
-                        return ListView(
-                          scrollDirection: Axis.horizontal,
-                          children: snapshot.data!.docs.map((e) {
-                            var foodModel = FoodModel.fromJson(
-                                (e.data() as Map<String, dynamic>));
+                const SizedBox(height: 20), // Adicione um espaço entre os menus
 
-                            return CardItemWidget(
-                              title: foodModel.title,
-                              description: foodModel.description,
-                              peoples: foodModel.peoples,
-                              price: foodModel.price,
-                              priceColor: const Color(0xFF2E9048),
+                // Pizzas
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      'Pizzas',
+                      style:
+                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                    ),
+
+                    // Content
+                    SizedBox(
+                        height: 270,
+                        child: StreamBuilder<QuerySnapshot>(
+                          stream: _foodService.getAllFoods('Pizzas'),
+                          builder: (context, snapshot) {
+                            return ListView(
+                              scrollDirection: Axis.horizontal,
+                              children: snapshot.data!.docs.map((e) {
+                                var foodModel = FoodModel.fromJson(
+                                    (e.data() as Map<String, dynamic>));
+
+                                return GestureDetector(
+                                  key: Key(foodModel.imageId),
+                                  onTap: () {
+                                    Navigator.push(context, MaterialPageRoute(
+                                      builder: (context) {
+                                        return DetailsPage(
+                                          foodModel: foodModel,
+                                        );
+                                      },
+                                    ));
+                                  },
+                                  child: CardItemWidget(
+                                    title: foodModel.title,
+                                    imageId: foodModel.imageId,
+                                    description: foodModel.description,
+                                    peoples: foodModel.peoples,
+                                    price: foodModel.price,
+                                  ),
+                                );
+                              }).toList(),
                             );
-                          }).toList(),
-                        );
-                      },
-                    )),
-              ],
-            ),
-
-            const SizedBox(height: 20), // Adicione um espaço entre os menus
-
-            // Pizzas
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Text(
-                  'Pizzas',
-                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                          },
+                        )),
+                  ],
+                ),
+                const SizedBox(
+                  height: 15,
                 ),
 
-                // Content
-                SizedBox(
-                    height: 270,
-                    child: StreamBuilder<QuerySnapshot>(
-                      stream: _foodService.getAllFoods('Pizzas'),
-                      builder: (context, snapshot) {
-                        return ListView(
-                          scrollDirection: Axis.horizontal,
-                          children: snapshot.data!.docs.map((e) {
-                            var foodModel = FoodModel.fromJson(
-                                (e.data() as Map<String, dynamic>));
+                // BEBIDAS
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      'Porções',
+                      style:
+                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                    ),
 
-                            return CardItemWidget(
-                              title: foodModel.title,
-                              description: foodModel.description,
-                              peoples: foodModel.peoples,
-                              price: foodModel.price,
+                    // Content
+                    SizedBox(
+                        height: 270,
+                        child: StreamBuilder<QuerySnapshot>(
+                          stream: _foodService.getAllFoods('Porções'),
+                          builder: (context, snapshot) {
+                            return ListView(
+                              scrollDirection: Axis.horizontal,
+                              children: snapshot.data!.docs.map((e) {
+                                var foodModel = FoodModel.fromJson(
+                                    (e.data() as Map<String, dynamic>));
+
+                                return GestureDetector(
+                                  key: Key(foodModel.imageId),
+                                  onTap: () {
+                                    Navigator.push(context, MaterialPageRoute(
+                                      builder: (context) {
+                                        return DetailsPage(
+                                          foodModel: foodModel,
+                                        );
+                                      },
+                                    ));
+                                  },
+                                  child: CardItemWidget(
+                                    title: foodModel.title,
+                                    imageId: foodModel.imageId,
+                                    description: foodModel.description,
+                                    peoples: foodModel.peoples,
+                                    price: foodModel.price,
+                                  ),
+                                );
+                              }).toList(),
                             );
-                          }).toList(),
-                        );
-                      },
-                    )),
-              ],
-            ),
-            const SizedBox(
-              height: 15,
-            ),
-
-            // BEBIDAS
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Text(
-                  'Bebidas',
-                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                          },
+                        )),
+                  ],
                 ),
 
-                // Content
-                SizedBox(
-                    height: 270,
-                    child: StreamBuilder<QuerySnapshot>(
-                      stream: _foodService.getAllFoods('Bebidas'),
-                      builder: (context, snapshot) {
-                        return ListView(
-                          scrollDirection: Axis.horizontal,
-                          children: snapshot.data!.docs.map((e) {
-                            var foodModel = FoodModel.fromJson(
-                                (e.data() as Map<String, dynamic>));
-
-                            return CardItemWidget(
-                              title: foodModel.title,
-                              description: foodModel.description,
-                              peoples: foodModel.peoples,
-                              price: foodModel.price,
-                            );
-                          }).toList(),
-                        );
-                      },
-                    )),
-              ],
-            ),
-
-            const SizedBox(
-              height: 15,
-            ),
-
-            // PORÇÕES
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Text(
-                  'Porções',
-                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                const SizedBox(
+                  height: 15,
                 ),
 
-                // Content
-                SizedBox(
-                    height: 270,
-                    child: StreamBuilder<QuerySnapshot>(
-                      stream: _foodService.getAllFoods('Porções'),
-                      builder: (context, snapshot) {
-                        return ListView(
-                          scrollDirection: Axis.horizontal,
-                          children: snapshot.data!.docs.map((e) {
-                            var foodModel = FoodModel.fromJson(
-                                (e.data() as Map<String, dynamic>));
+                // PORÇÕES
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      'Bebidas',
+                      style:
+                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                    ),
 
-                            return CardItemWidget(
-                              title: foodModel.title,
-                              description: foodModel.description,
-                              peoples: foodModel.peoples,
-                              price: foodModel.price,
+                    // Content
+                    SizedBox(
+                        height: 270,
+                        child: StreamBuilder<QuerySnapshot>(
+                          stream: _foodService.getAllFoods('Bebidas'),
+                          builder: (context, snapshot) {
+                            return ListView(
+                              scrollDirection: Axis.horizontal,
+                              children: snapshot.data!.docs.map((e) {
+                                var foodModel = FoodModel.fromJson(
+                                    (e.data() as Map<String, dynamic>));
+
+                                return GestureDetector(
+                                  key: Key(foodModel.imageId),
+                                  onTap: () {
+                                    Navigator.push(context, MaterialPageRoute(
+                                      builder: (context) {
+                                        return DetailsPage(
+                                          foodModel: foodModel,
+                                        );
+                                      },
+                                    ));
+                                  },
+                                  child: CardItemWidget(
+                                    title: foodModel.title,
+                                    imageId: foodModel.imageId,
+                                    description: foodModel.description,
+                                    peoples: foodModel.peoples,
+                                    price: foodModel.price,
+                                  ),
+                                );
+                              }).toList(),
                             );
-                          }).toList(),
-                        );
-                      },
-                    )),
+                          },
+                        )),
+                  ],
+                ),
               ],
-            ),
+            )
           ],
         ),
       ),
