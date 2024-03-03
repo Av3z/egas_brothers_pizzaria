6c73caf3f150bbac0e5ed734ce27294e7679a639
Av3z <willyanf17@hotmail.com>
2024-03-02 23:20:47 -0300

    refactor: add pizza section

--- lib/src/pages/details_page.dart
+++ lib/src/pages/details_page.dart
@@ -1,4 +1,5 @@
 import 'package:egas_brothers_pizzaria/src/components/details_components/item_details.dart';
+import 'package:egas_brothers_pizzaria/src/components/details_components/pizza_section.dart';
 import 'package:egas_brothers_pizzaria/src/components/primary_button_widget.dart';
 import 'package:egas_brothers_pizzaria/src/models/food_model.dart';
 import 'package:egas_brothers_pizzaria/src/services/cart_service.dart';
@@ -146,49 +147,7 @@ class _DetailsPageState extends State<DetailsPage> {
                   ),
                   !(widget.foodModel.category == "Pizzas")
                       ? Container()
-                      : Column(
-                          children: [
-                            Container(
-                              alignment: Alignment.topLeft,
-                              child: const Text(
-                                'Escolha até 4 sabores:',
-                                style: TextStyle(
-                                    fontSize: 16, fontWeight: FontWeight.bold),
-                              ),
-                            ),
-                            const ItemDetails(title: 'Calabresa'),
-                            const ItemDetails(title: 'Bacon'),
-                            const ItemDetails(title: 'Coração'),
-                            const ItemDetails(title: 'Frango com Catupiry'),
-                            const ItemDetails(title: '4 Queijos'),
-                            const ItemDetails(title: 'Portuguesa'),
-                            const ItemDetails(title: 'Strogonoff'),
-                            const ItemDetails(title: 'Fricassê'),
-                            const ItemDetails(title: 'Camarão'),
-                            const ItemDetails(title: 'Picanha'),
-                            const ItemDetails(title: 'Chocolate Preto'),
-                            const ItemDetails(title: 'Chocolate Branco'),
-                            const ItemDetails(title: 'Nutella com M&M'),
-                            const ItemDetails(title: 'Nutella com Morango'),
-                            const ItemDetails(title: 'Negresco'),
-                            const ItemDetails(title: 'Prestígio'),
-                            const SizedBox(
-                              height: 30,
-                            ),
-                            Container(
-                              alignment: Alignment.topLeft,
-                              child: const Text(
-                                'Sabor da borda:',
-                                style: TextStyle(
-                                    fontSize: 16, fontWeight: FontWeight.bold),
-                              ),
-                            ),
-                            const ItemDetails(title: 'Sem borda'),
-                            const ItemDetails(title: 'Cheddar'),
-                            const ItemDetails(title: 'Mussarela'),
-                            const ItemDetails(title: 'Catupiry'),
-                          ],
-                        ),
+                      : const PizzaSection(),
                   const SizedBox(
                     height: 30,
                   ),
